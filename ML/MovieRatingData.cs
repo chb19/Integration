742ca673e0da838f934fd90c9ed14138000ec52d
Bohdan Porokhnavets <bogdanpro7@gmail.com>
2023-05-18 11:37:21 +0300

    Added ML

---
+++ ML/MovieRatingData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML.Data;
+
+namespace ML
+{
+    public class NewsRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float NewsId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+
+    public class NewsRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
